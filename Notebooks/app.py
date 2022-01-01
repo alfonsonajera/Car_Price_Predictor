8b9166a97db77bb4d0bdab84aab1df62b91fbe55
alfonsonajera <82969582+alfonsonajera@users.noreply.github.com>
2022-01-02 00:44:12 +0100

    Update app.py

--- Notebooks/app.py
+++ Notebooks/app.py
@@ -6,8 +6,8 @@ from category_encoders import TargetEncoder
 import bz2
 import _pickle as cPickle
 
-
-
+compressed_model = bz2.BZ2File("Notebooks/RF_price_predicting_model.pkl.pbz2", 'rb')
+model = cPickle.load(compressed_model)
 
 
 
@@ -17,9 +17,15 @@ This app predicts **Used Cars Value**!
 """)
 st.write('---')
 
+# Sidebar
+# Header of Specify Input Parameters
+st.sidebar.header('Specify Input Parameters')
 
-compressed_model = bz2.BZ2File("Notebooks/RF_price_predicting_model.pkl.pbz2", 'rb')
-model = cPickle.load(compressed_model)
+# Loads the Used Cars Dataset
+cars_final = pd.read_csv('Csv/03.cars_final_def.csv')
+cars_final = cars_final.drop(['Version', 'ZIP'], axis=1)
+X= cars_final[cars_final.columns[:-1]]
+y= cars_final[cars_final.columns[-1]]
 
 
 
