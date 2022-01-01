976da8a8c29e07d0f7fbb92860a7c166497da616
alfonsonajera <82969582+alfonsonajera@users.noreply.github.com>
2022-01-02 00:36:30 +0100

    Update app.py

--- Notebooks/app.py
+++ Notebooks/app.py
@@ -3,17 +3,16 @@ import pickle
 import pandas as pd
 import numpy as np
 from category_encoders import TargetEncoder
+import bz2
+import _pickle as cPickle
+
+
+
+
+
 
 st.write("""
 # Car Price Predictor
 This app predicts **Used Cars Value**!
 """)
 st.write('---')
-
-# Sidebar
-# Header of Specify Input Parameters
-st.sidebar.header('Specify Input Parameters')
-
-
-
-model = pickle.load(open('Notebooks/RF_price_predicting_model.pkl','rb'))
