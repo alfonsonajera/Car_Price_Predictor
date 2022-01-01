39f863ba574ecf1646a01e8ea6f3bc3c400f5d15
alfonsonajera <82969582+alfonsonajera@users.noreply.github.com>
2022-01-01 22:28:37 +0100

    Update app.py

--- Notebooks/app.py
+++ Notebooks/app.py
@@ -13,7 +13,6 @@ st.sidebar.header('Specify Input Parameters')
 import pickle
 import pandas as pd
 import numpy as np
-from category_encoders import TargetEncoder
 
 model = pickle.load(open('/users/alfon/Desktop/Master/TFM/Notebooks/RF_price_predicting_model.pkl','rb'))
 
