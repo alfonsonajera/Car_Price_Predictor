000afdc684859745857a44ac182980894f27c6be
alfonsonajera <82969582+alfonsonajera@users.noreply.github.com>
2022-01-01 18:32:10 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,2 @@
 import streamlit as st
 import pickle
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn import datasets
-from sklearn.ensemble import RandomForestRegressor
