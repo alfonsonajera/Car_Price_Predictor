9ca1bd1e9abf9e1c31530bfffaa5da35e1da2c16
alfonsonajera <82969582+alfonsonajera@users.noreply.github.com>
2022-01-02 13:15:55 +0100

    Update app.py

--- Notebooks/app.py
+++ Notebooks/app.py
@@ -35,7 +35,7 @@ def user_input_features():
     MODEL = st.sidebar.selectbox('Model', np.sort(cars_final[cars_final.Brand == BRAND].Model.unique()), index=0)
         
     YEAR = st.sidebar.slider('Year', int(X.Year.min()), int(X.Year.max()), int(X.Year.mean()))
-    KMS = st.sidebar.number_input('Kms', 0, 1000000, int(X.Kms.mean()), step = 10000)
+    KMS = st.sidebar.number_input('Kms', 0, 1000000, int(X.Kms.mean()), step = 1)
     HP = st.sidebar.slider('Power(Hp)', 1, 1000, int(X.Hp.mean()))
     TRANSMISSION = st.sidebar.selectbox('Transmission', X.Gear_type.unique())
     FUEL = st.sidebar.selectbox('Fuel type', cars_final.Fuel_type.unique(), index=0)
