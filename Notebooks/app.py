6e4fd11f6df46d12a14cf010f7c2b704b697e61b
alfonsonajera <82969582+alfonsonajera@users.noreply.github.com>
2022-01-02 13:14:37 +0100

    Update app.py

--- Notebooks/app.py
+++ Notebooks/app.py
@@ -31,7 +31,7 @@ y= cars_final[cars_final.columns[-1]]
 def user_input_features():
     
     
-    BRAND = st.sidebar.selectbox('Brand', np.sort(cars_final.Brand.unique()), index=9)
+    BRAND = st.sidebar.selectbox('Brand', np.sort(cars_final.Brand.unique()), index=8)
     MODEL = st.sidebar.selectbox('Model', np.sort(cars_final[cars_final.Brand == BRAND].Model.unique()), index=0)
         
     YEAR = st.sidebar.slider('Year', int(X.Year.min()), int(X.Year.max()), int(X.Year.mean()))
@@ -49,7 +49,7 @@ def user_input_features():
     
     
     PROVINCE = st.sidebar.selectbox('Province', cars_final.Province.unique(), index=0)
-    SELLER = st.sidebar.radio("Seller", (Dealer, Private), key='owner')
+    SELLER = st.sidebar.radio("Seller", ("Dealer", "Private"))
     
     
 
