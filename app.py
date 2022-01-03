0c584a0552d7997d3991e127f1c5086cca68a4e4
alfonsonajera <82969582+alfonsonajera@users.noreply.github.com>
2022-01-03 20:59:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ def main():
     prediction = pd.DataFrame(model.predict(df_pred))
     prediction.columns = ['Price_EUR']
     prediction = prediction.round(decimals = 0)
-    prediction['Price_EUR'] = prediction['Price_EUR'].astype(str).apply(lambda x: x.replace('.0',' €'))
+    prediction['Price_EUR'] = prediction['Price_EUR'].astype(str).apply(lambda x: x.replace('.0',''))
     
   
   
