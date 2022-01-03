6cac274ab9ec0a5f69e1c7c10b87d4b4cb87e287
alfonsonajera <82969582+alfonsonajera@users.noreply.github.com>
2022-01-04 00:25:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.write("""
 # VhehiCALC
 This app predicts **Used Cars Values**!
 """)
-st.image("vehicalc.png",use_column_width=False)
+st.image("vehicalc.png",use_column_width=True)
 st.write('---')
 
 # Sidebar
