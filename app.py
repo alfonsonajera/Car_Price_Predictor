40cdecc68c38c4b06f063be8befcceae5fbe9ae4
alfonsonajera <82969582+alfonsonajera@users.noreply.github.com>
2022-01-04 00:45:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ col1, col2 = st.beta_columns([1, 20])
 image = "vehicalc.png"
 with title_container:
         with col1:
-                st.image(image, width=64)
+                st.image(image, use_column_width=False)
         with col2:
                 st.markdown('<h1 style="color: purple;">Suzieq</h1>',
                             unsafe_allow_html=True)
