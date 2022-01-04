18a8ce564ddd84e77f4c3fd9161ea4e503605e30
alfonsonajera <82969582+alfonsonajera@users.noreply.github.com>
2022-01-04 22:35:16 +0100

    Update vehicalc-app.py

--- vehicalc-app.py
+++ vehicalc-app.py
@@ -253,6 +253,15 @@ def main():
     shap_values = explainer.shap_values(df_pred)
     
     col1, col2= st.columns(2)
+        
+    with col1:
+        st.set_option('deprecation.showPyplotGlobalUse', False)
+        plt.title('Prediction Feature importance based on SHAP values (Bar)')
+        shap.summary_plot(shap_values, df_pred, plot_type="bar")
+        st.pyplot(bbox_inches='tight')
+    
+    with col2:
+        st.image("Figs/0.7_Shap_summary_plot.png",
 
 
     
