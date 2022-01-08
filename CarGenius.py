b4a53e9872b0acebfcc0befa318fea5b7460cb19
alfonsonajera <82969582+alfonsonajera@users.noreply.github.com>
2022-01-08 16:09:59 +0100

    Update CarGenius.py

--- CarGenius.py
+++ CarGenius.py
@@ -11,27 +11,7 @@ import shap
 compressed_model = bz2.BZ2File("Files/RF_price_predicting_model.pkl.pbz2", 'rb')
 model = cPickle.load(compressed_model)
 
-st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 
-st.markdown(""" 
-<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #000000;">
-  <a class="navbar-brand" href="#" target="_blank">Car Genius</a> 
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="https://github.com/alfonsonajera/Car_Price_Predictor" target="_blank">GitHub</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="https://www.linkedin.com/in/alfonso-n%C3%A1jera-del-barrio-66b926124/" target="_blank">LinkdIn</a>
-      </li>
-    </ul>
-  </div>
-</nav>
-
-""", unsafe_allow_html=True)
 # Title logo
 st.image("CarGenius_header.png")
 st.write(""" 
@@ -47,7 +27,7 @@ side_bar = """
   <style>
     /* The whole sidebar */
     .css-1lcbmhc.e1fqkh3o0{
-      margin-top: 10rem;
+      margin-top: 3.8rem;
     }
      
      /* The display arrow */
@@ -294,7 +274,27 @@ def main():
 
 
     st.write('---')
-     
+    st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
+
+st.markdown(""" 
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #000000;">
+  <a class="navbar-brand" href="#" target="_blank">Car Genius</a> 
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="https://github.com/alfonsonajera/Car_Price_Predictor" target="_blank">GitHub</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="https://www.linkedin.com/in/alfonso-n%C3%A1jera-del-barrio-66b926124/" target="_blank">LinkdIn</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+""", unsafe_allow_html=True) 
   
         
 
