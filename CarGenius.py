91fec1ac42cec5e95aa987dfb7af680c933e780e
alfonsonajera <82969582+alfonsonajera@users.noreply.github.com>
2022-01-08 15:23:46 +0100

    Update CarGenius.py

--- CarGenius.py
+++ CarGenius.py
@@ -15,7 +15,7 @@ st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootst
 
 st.markdown("""
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #000000;">
-  <a class="navbar-brand" href="https://youtube.com/dataprofessor" target="_blank">st.image("CarGenius_header.png")</a>
+  <a class="navbar-brand" target="_blank">CarGenius</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
