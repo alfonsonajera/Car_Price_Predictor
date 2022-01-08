446fefe6b94c1a89f33509410725be97029f236d
alfonsonajera <82969582+alfonsonajera@users.noreply.github.com>
2022-01-08 15:28:28 +0100

    Update CarGenius.py

--- CarGenius.py
+++ CarGenius.py
@@ -13,7 +13,7 @@ model = cPickle.load(compressed_model)
 
 st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 
-st.markdown("""
+st.markdown(""" ![Cool Image](cargenius.png)
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #000000;">
   <a class="navbar-brand" href="#" target="_blank">CarGenius</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
