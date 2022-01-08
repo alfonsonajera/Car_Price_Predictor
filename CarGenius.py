4dc0f823ff14baee20c061aa6faf89d2afac3144
alfonsonajera <82969582+alfonsonajera@users.noreply.github.com>
2022-01-08 14:49:23 +0100

    Update CarGenius.py

--- CarGenius.py
+++ CarGenius.py
@@ -44,6 +44,21 @@ st.write('---')
 
 # Sidebar
 # Header of Specify Input Parameters
+
+side_bar = """
+  <style>
+    /* The whole sidebar */
+    .css-1lcbmhc.e1fqkh3o0{
+      margin-top: 3.8rem;
+    }
+     
+     /* The display arrow */
+    .css-sg054d.e1fqkh3o3 {
+      margin-top: 5rem;
+      }
+  </style> 
+  """
+st.markdown(side_bar, unsafe_allow_html=True)
 st.sidebar.header('Specify Input Parameters')
 
     
